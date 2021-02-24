c8bb3154c97e8ffefe60f330c0190d35d10a31e2
samsuccess <samsuccess36@gmail.com>
2021-02-24 22:36:07 +0300

    change homework1

--- app/src/main/java/ru/example/myapplication/MainActivity.kt
+++ app/src/main/java/ru/example/myapplication/MainActivity.kt
@@ -1,12 +1,35 @@
 package ru.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        click()
+        newClick()
     }
+
+    private val outs = Output("Привет, я Игорь!", "Мне 43 года!");
+    val newcopy = outs.copy(text = "Я из Ярославля!" )
+    private fun click() {
+        val button: Button = findViewById(R.id.click)
+        button.setOnClickListener {
+            val text = findViewById<TextView>(R.id.answer)
+            val age = findViewById<TextView>(R.id.age)
+            text.text = outs.text
+            age.text = outs.age
+        }
+    }
+    private fun newClick(){
+        val button: Button = findViewById(R.id.new_click)
+        button.setOnClickListener {
+            val copy = findViewById<TextView>(R.id.new_copy)
+            copy.text = newcopy.text
+        }
+    }
+
 }
---
+++ app/src/main/java/ru/example/myapplication/Output.kt
@@ -0,0 +1,4 @@
+package ru.example.myapplication
+
+data class Output (val text:String, val age: String)
+
